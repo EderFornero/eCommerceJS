2bb92ad24f4cf39e9910a20d38a39b6366bbb2c1
EderFornero <ederfornero.ef@gmail.com>
2022-06-13 22:06:14 -0300

    while-creado

---
+++ index.js
@@ -0,0 +1,45 @@
+
+
+// for (desde; hasta; actualizacion;){}
+
+// for(let i = 1; i <= 100; i = i + 1){ 
+//     console.log("numeritos", i);
+// }
+
+// for (let i = 1; i <=10; i++){ 
+//     if(i === 6){ 
+//         break
+//     }
+//     console.log(i);
+// }
+
+// for (let i = 1; i <=10; i++){ 
+//     if(i === 6){ 
+//         continue
+//     }
+//     console.log(i);
+// }
+
+
+// ESTRUCTURA CON WHILE 
+
+let cuenta = parseInt(prompt("Ingrese su numero de cuenta"));
+let intentos = 0; 
+
+while(cuenta != "02"){ 
+    alert("Cuenta no registrada"); 
+
+    if(intentos === 3){ 
+      break; 
+    }
+
+    intentos++
+    prompt("Ingrese su numero de cuenta")
+}
+
+if(intentos === 3){
+    alert("Vuelva a intentar en 5 minutos");
+    prompt("Ingrese su numero de cuenta"); 
+}else(
+    alert("Cuenta registrada")
+);
