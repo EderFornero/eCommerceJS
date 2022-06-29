521bb11ea2b15557c57252c3b523f753c2be2e5f
EderFornero <ederfornero.ef@gmail.com>
2022-06-29 19:18:47 -0300

    primera entrega

--- JS/index.js
+++ JS/index.js
@@ -155,30 +155,30 @@ nombreFuncion("parentesis vacios es decir que se ejecuta");
 
 // "SPLICE" SE USA PARA ELIMINAR PARAMETROS DESDE UNA POSICION SELECCIONADA HASTA OTRA POSICION SELECCIONADA
 
-const array3 = ["Juan", "Pepe", "Tomas", "Mariana", "Carlos", "Jose"]; 
+// const array3 = ["Juan", "Pepe", "Tomas", "Mariana", "Carlos", "Jose"]; 
 
-array3.splice(6); 
-console.log(array3);
+// array3.splice(6); 
+// console.log(array3);
 
-// TAMBIEN SE PUEDE UTILIZAR PARA CAMBIAR 
+// // TAMBIEN SE PUEDE UTILIZAR PARA CAMBIAR 
 
-{const array3 = ["Juan", "Pepe", "Tomas", "Mariana", "Carlos", "Jose"]; 
+// {const array3 = ["Juan", "Pepe", "Tomas", "Mariana", "Carlos", "Jose"]; 
 
-array3.splice(4, 1, 'josesito'); 
-console.log(array3);}
+// array3.splice(4, 1, 'josesito'); 
+// console.log(array3);}
 
-{
-const array3 = ["Juan", "Pepe", "Tomas", "Mariana", "Carlos", "Jose"]; 
-array3.splice(1, 3, 'josesito', 'pepito', 'jorgito');
-console.log(array3);}
+// {
+// const array3 = ["Juan", "Pepe", "Tomas", "Mariana", "Carlos", "Jose"]; 
+// array3.splice(1, 3, 'josesito', 'pepito', 'jorgito');
+// console.log(array3);}
 
 
 
 
 // "indexOF" busca la posicion del elemento pedido, si el mismo no existe retorna -1
-{
-const array3 = ["Juan", "Pepe", "Tomas", "Mariana", "Pensar", "Ejemplos", "Largos"];
-console.log(array3.indexOf("Largos")); }
+// {
+// const array3 = ["Juan", "Pepe", "Tomas", "Mariana", "Pensar", "Ejemplos", "Largos"];
+// console.log(array3.indexOf("Largos")); }
 
 
 
@@ -186,61 +186,184 @@ console.log(array3.indexOf("Largos")); }
 
 // "includes" para ver si esta o no dicho parametro
 
-{
-const array3 = ["Juan", "Pepe", "Tomas", "Mariana", "Pensar", "Ejemplos", "Largos"];
-console.log(array3.includes("Juana")); //false
-console.log(array3.includes("Juan")); //true
-}
+// {
+// const array3 = ["Juan", "Pepe", "Tomas", "Mariana", "Pensar", "Ejemplos", "Largos"];
+// console.log(array3.includes("Juana")); //false
+// console.log(array3.includes("Juan")); //true
+// }
 
 
 
 
 
 // "reverse" para modificar en reversa el array
-{
-const array3 = ["Maria", "Jose", "Pepe", "Jorge"]; 
-array3.reverse(); 
-console.log(array3);}
+// {
+// const array3 = ["Maria", "Jose", "Pepe", "Jorge"]; 
+// array3.reverse(); 
+// console.log(array3);}
 
 
 
 
 //RECORRER UN ARRAY 
 
-const productos = [
-  {id: 1, nombre: "Gabinete"},
-  {id: 2, nombre: "GPU"}, 
-  {id: 3, nombre: "RAM"}, 
-]; 
+// const productos = [
+//   {id: 1, nombre: "Gabinete"},
+//   {id: 2, nombre: "GPU"}, 
+//   {id: 3, nombre: "RAM"}, 
+// ]; 
 
-for (const producto of productos){
-console.log(producto.id)
-console.log(producto.nombre);}; 
+// for (const producto of productos){
+// console.log(producto.id)
+// console.log(producto.nombre);}; 
 
 
 
 
 //OTRA FORMA ES CON FUNCION CONST
 
-class Animales { 
-    constructor(name, amount, value){ 
-      this.name = name; 
-      this.amount = amount; 
-      this.value = value; 
-    }
+// class Animales { 
+//     constructor(name, amount, value){ 
+//       this.name = name; 
+//       this.amount = amount; 
+//       this.value = value; 
+//     }
+//   }
+  
+//   const animales = []; 
+  
+//   animales.push (new Animales ("Cat", 3, "2000"));
+//   animales.push (new Animales ("Dog", 2, "3000"));
+//   animales.push (new Animales ("Horse", 5, "6000"));
+  
+//   console.table(animales);
+  
+
+
+
+
+
+  
+{
+const year = prompt("¿En qué año ganó Argentina su última copa américa?"); 
+
+if (year == 2021){ 
+   alert("¡Correcto!"); 
+}else{ alert("¡Eso no es verdad!")}
+}
+
+
+{
+let vocales = prompt("Ingrese una vocal"); 
+
+while(vocales != "a", "e", "i", "o", "u"){ 
+  switch(volcales){ 
+    case "a".toLocaleLowerCase: 
+    alert("Ingresaste la vocal A");
+    break; 
+
+    case "e".toLocaleLowerCase: 
+    alert("Ingresaste la vocal E");
+    break; 
+
+    case "i".toLocaleLowerCase: 
+    alert("Ingresaste la vocal I");
+    break; 
+
+    case "o".toLocaleLowerCase: 
+    alert("Ingresaste la vocal O");
+    break; 
+
+    case "u".toLocaleLowerCase: 
+    alert("Ingresaste la vocal U");
+    break; 
+
+    default: alert("No es una vocal"); 
+    break;
   }
-  
-  const animales = []; 
-  
-  animales.push (new Animales ("Cat", 3, "2000"));
-  animales.push (new Animales ("Dog", 2, "3000"));
-  animales.push (new Animales ("Horse", 5, "6000"));
-  
-  console.table(animales);
-  
-  
-  
-  
+}
+
+}
+
+{
+
+const resta = (a,b) => a - b; 
+console.log(resta(202,58));
+
+const suma = (a, b, c) => a + b + c; 
+console.log(suma(50,60,100));
+
+const multiplicacion = (a, b, c) => a * b * c; 
+console.log(multiplicacion(20, 15, 7));
+
+const division = (a, b) => a / b; 
+console.log(division(206, 21));
+}
+
+
+{
+console.log(Math.max(50, 500, 12, 45));
+console.log(Math.min(50, 500, 12, 45));
+console.log(Math.PI);
+}
 
 
 
+
+
+{
+let numero = "Ingrese un número"; 
+let repeticiones = 0; 
+let corte = 10; 
+let detener = false; 
+
+while (repeticiones <= numero && detener==false){ 
+
+  if(repeticiones>corte){ 
+    console.log("Se corta el ciclo");
+    detener===true;
+    break;
+  }else
+  { 
+    console.log("El ciclo continua");
+  }
+}
+
+}
+
+
+
+{
+
+const array1 = [
+  {producto: "1" ,nombre: "CPU", marca: "Ryzen"},
+  {producto: "2" , nombre: "Mother", tamaño: "Medium"},
+  {producto: "3", nombre: "GPU", tipo: "Nvidia"}, 
+]
+
+for(const productos of array1){ 
+  console.log(productos);
+}
+
+}
+
+
+{
+
+class Stock{
+  constructor(nombre, precio, cantidad){ 
+  this.nombre = nombre; 
+  this.precio = precio; 
+  this.cantidad = cantidad; 
+  }
+ }
+
+const array2 = []; 
+
+array2.push (new Stock("CPU", "$40000", 2));
+array2.push (new Stock("Mother", "$21000", 5));
+array2.push (new Stock("GPU", "$80000", 3));
+
+console.log(array2);
+
+}
