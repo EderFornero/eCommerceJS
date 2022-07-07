501206cef7e13da973b940321159df989479ff8a
EderFornero <ederfornero.ef@gmail.com>
2022-07-07 19:40:52 -0300

    div

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,7 @@
 //Search
-let search = document.getElementById("search"); 
-search.addEventListener("keydown", (e) => { 
-    e.preventDefault(); 
+let search = document.getElementById("search");
+search.addEventListener("keydown", (e) => {
+    e.preventDefault();
     console.log(search);
 })
 
@@ -30,20 +30,24 @@ formInput.addEventListener("keydown", (e) => {
 })
 
 //Stock---------------------------------------------------------------------------------------------
-const contenedorProductos = document.getElementById("contenedor-productos");
-
-let stockProductos = [
+const contenedorProductos = document.getElementById("contenedor-productosGPU");
+const contenedorProductosCPU = document.getElementById("contenedor-productosCPU")
+let stockGPU = [
     { id: "1", name: "GIGABYTE GEFORCE GTX 1660", brand: "Nvidia", price: "595,43", img: `./images/Productos/1660.jpg` },
     { id: "2", name: "GIGABYTE GEFORCE RTX 2070 SUPER WINDFORCE OC", brand: "Nvidia", price: "1000", img: `./images/Productos/2070.jpg` },
     { id: "3", name: "MSI Radeon RX 570 Armor ARMOR 8G OC", brand: "AMD", price: "714,52", img: `./images/Productos/570.webp` },
     { id: "4", name: "Sapphire Radeon RX 580 Nitro Plus", brand: "AMD", price: "952,69", img: `./images/Productos/580.jpg` },
-    { id: "5", name: "GIGABYTE GEFORCE RTX 2080 SUPER WINDFORCE OC", brand: "Nvidia", price: "1032,08", img: `./images/Productos/2080.jpg` },
-    { id: "6", name: "MSI GeForce GTX 1050 Ti Gaming X 4G", brand: "Nvidia", price: "541,44", img: `./images/Productos/1050ti.jpg` },
-
 ];
 
+let stockCPU = [
+    { id: "1", name: "Ryzen 5 5600G", brand: "AMD", price: "276,47", img: `./images/Productos/AMD-5600g.jpg`},
+    { id: "2", name: "Ryzen 5 5600X", brand: "AMD", price: "400,72", img: `./images/Productos/AMD-5600x.jpg`},
+    { id: "4", name: "Core i5-10600KF", brand: "Intel", price: "308,64", img: `./images/Productos/CoreI5-10dc.jpg` },
+    { id: "5", name: "Core i5-11600KF", brand: "Intel", price: "363,50", img: `./images/Productos/CPU-CoreI5-11va.webp`},
+]
 
-stockProductos.forEach((producto) => {
+
+stockGPU.forEach((producto) => {
     const div = document.createElement("div")
     div.classList.add("producto")
     div.innerHTML = `
@@ -56,11 +60,23 @@ stockProductos.forEach((producto) => {
     contenedorProductos.appendChild(div);
 });
 
+stockCPU.forEach((producto) => { 
+    const div = document.createElement("div")
+    div.classList.add("productoCPU")
+    div.innerHTML = `
+    <img class="img-producto" src =${producto.img} alt= ""> 
+    <h3 class="nameProducto">${producto.name}</h3>
+    <p>Marca: ${producto.brand}</p>
+    <p class="precioProducto">$${producto.price}</p>
+    `
+  contenedorProductosCPU.appendChild(div);
+   
+})
 //---------------------------------------------------------------------------------------------
 
 //localStorage
 
-localStorage.setItem("stock", JSON.stringify(stockProductos));
+localStorage.setItem("stock", JSON.stringify(stockGPU));
 
 
 //--------------------------------------------------------------
