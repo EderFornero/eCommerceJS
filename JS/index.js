51009a29218d83639d1d93c64fed279ae4eb84af
EderFornero <ederfornero.ef@gmail.com>
2022-07-06 22:26:10 -0300

    localStorage

--- JS/index.js
+++ JS/index.js
@@ -1,16 +1,26 @@
 
-//Navbar
-
-  
 
 
-//--------------------------------------------------------------
+
+let formulario = document.getElementById("formulario"); 
+formulario.addEventListener("submit", reconocimiento); 
+
+function reconocimiento (e) { 
+    e.preventDefault();
+
+    let form = e.target; 
+    
+    let username = form.children[0].value;
+    console.log(username);
+
+}
+
 
 
 const contenedorProductos = document.getElementById("contenedor-productos"); 
 
 let stockProductos = [ 
-    {id: "1", name: "GIGABYTE GEFORCE GTX 1660", brand: "Nvidia", price: "595,43", img: `./images/Productos/producto.png`},
+    {id: "1", nameA: "GIGABYTE GEFORCE GTX 1660", brand: "Nvidia", price: "595,43", img: `./images/Productos/producto.png`},
     {id: "2", name: "GIGABYTE GEFORCE RTX 2070 SUPER WINDFORCE OC", brand: "Nvidia", price: "1000", img: `./images/Productos/producto.png`},
     {id: "3", name: "MSI Radeon RX 570 Armor ARMOR 8G OC", brand: "AMD", price: "714,52", img: `./images/Productos/producto.png`},
     {id: "4", name: "Sapphire Radeon RX 580 Nitro Plus", brand: "AMD", price: "952,69", img: `./images/Productos/producto.png`},
@@ -18,6 +28,7 @@ let stockProductos = [
     {id: "6", name: "MSI GeForce GTX 1050 Ti Gaming X 4G", brand: "Nvidia", price: "541,44", img: `./images/Productos/producto.png`},
    
    ]; 
+
 
 stockProductos.forEach((producto) => { 
     const div = document.createElement ("div")
@@ -34,8 +45,27 @@ stockProductos.forEach((producto) => {
 
 
 
-  
+//localStorage
+
+localStorage.setItem("stock", JSON.stringify(stockProductos));
+localStorage.setItem("user", JSON.stringify(formulario));  
+
+//--------------------------------------------------------------
   
 
+// localStorage.setItem("number", "25");
+// localStorage.setItem("number1", "29");
+// localStorage.setItem("number2", "30");
+// localStorage.setItem("number3", "57");
 
+
+
+// let persona = {nombre: "eder", edad: "19"}; 
+// localStorage.setItem("persona", JSON.stringify(persona))
+
+
+// let persona = JSON.parse(localStorage.getItem("persona"));
+// console.log(persona);
+
+// localStorage.clear();
 
