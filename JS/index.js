b22d831b0117d898d4ffe01da6a1dd697f19afdc
EderFornero <ederfornero.ef@gmail.com>
2022-07-07 17:49:37 -0300

    addEvent

--- JS/index.js
+++ JS/index.js
@@ -1,37 +1,45 @@
 
 
+//Formulario---------------------------------------------------------------------------------------
 
+let formInput = document.getElementById('formInput');
+let formulario = document.getElementById("formulario");
+formulario.addEventListener("submit", reconocimiento);
 
-let formulario = document.getElementById("formulario"); 
-formulario.addEventListener("submit", reconocimiento); 
-
-function reconocimiento (e) { 
+function reconocimiento(e) {
     e.preventDefault();
 
-    let form = e.target; 
-    
-    let username = form.children[0].value;
-    console.log(username);
+    console.log(formInput.value);
+    localStorage.setItem("user", JSON.stringify(formInput.value));
+
 
 }
 
+const saludo = document.getElementById("saludo");
+
+formInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        saludo.className = "green";
+    }
+
+})
+
+//Stock---------------------------------------------------------------------------------------------
+const contenedorProductos = document.getElementById("contenedor-productos");
+
+let stockProductos = [
+    { id: "1", name: "GIGABYTE GEFORCE GTX 1660", brand: "Nvidia", price: "595,43", img: `./images/Productos/1660.jpg` },
+    { id: "2", name: "GIGABYTE GEFORCE RTX 2070 SUPER WINDFORCE OC", brand: "Nvidia", price: "1000", img: `./images/Productos/2070.jpg` },
+    { id: "3", name: "MSI Radeon RX 570 Armor ARMOR 8G OC", brand: "AMD", price: "714,52", img: `./images/Productos/570.webp` },
+    { id: "4", name: "Sapphire Radeon RX 580 Nitro Plus", brand: "AMD", price: "952,69", img: `./images/Productos/580.jpg` },
+    { id: "5", name: "GIGABYTE GEFORCE RTX 2080 SUPER WINDFORCE OC", brand: "Nvidia", price: "1032,08", img: `./images/Productos/2080.jpg` },
+    { id: "6", name: "MSI GeForce GTX 1050 Ti Gaming X 4G", brand: "Nvidia", price: "541,44", img: `./images/Productos/1050ti.jpg` },
+
+];
 
 
-const contenedorProductos = document.getElementById("contenedor-productos"); 
-
-let stockProductos = [ 
-    {id: "1", nameA: "GIGABYTE GEFORCE GTX 1660", brand: "Nvidia", price: "595,43", img: `./images/Productos/producto.png`},
-    {id: "2", name: "GIGABYTE GEFORCE RTX 2070 SUPER WINDFORCE OC", brand: "Nvidia", price: "1000", img: `./images/Productos/producto.png`},
-    {id: "3", name: "MSI Radeon RX 570 Armor ARMOR 8G OC", brand: "AMD", price: "714,52", img: `./images/Productos/producto.png`},
-    {id: "4", name: "Sapphire Radeon RX 580 Nitro Plus", brand: "AMD", price: "952,69", img: `./images/Productos/producto.png`},
-    {id: "5", name: "GIGABYTE GEFORCE RTX 2080 SUPER WINDFORCE OC", brand: "Nvidia", price: "1032,08", img: `./images/Productos/producto.png`},
-    {id: "6", name: "MSI GeForce GTX 1050 Ti Gaming X 4G", brand: "Nvidia", price: "541,44", img: `./images/Productos/producto.png`},
-   
-   ]; 
-
-
-stockProductos.forEach((producto) => { 
-    const div = document.createElement ("div")
+stockProductos.forEach((producto) => {
+    const div = document.createElement("div")
     div.classList.add("producto")
     div.innerHTML = `
     <img class="img-producto" src =${producto.img} alt= ""> 
@@ -43,29 +51,13 @@ stockProductos.forEach((producto) => {
     contenedorProductos.appendChild(div);
 });
 
-
+//---------------------------------------------------------------------------------------------
 
 //localStorage
 
 localStorage.setItem("stock", JSON.stringify(stockProductos));
-localStorage.setItem("user", JSON.stringify(formulario));  
+
 
 //--------------------------------------------------------------
-  
 
-// localStorage.setItem("number", "25");
-// localStorage.setItem("number1", "29");
-// localStorage.setItem("number2", "30");
-// localStorage.setItem("number3", "57");
-
-
-
-// let persona = {nombre: "eder", edad: "19"}; 
-// localStorage.setItem("persona", JSON.stringify(persona))
-
-
-// let persona = JSON.parse(localStorage.getItem("persona"));
-// console.log(persona);
-
-// localStorage.clear();
 
